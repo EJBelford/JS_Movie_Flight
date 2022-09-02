bd0d6364c0669b01cec31b40931fa54035554b67
Gene Belford <35238105+EJBelford@users.noreply.github.com>
2022-09-02 15:40:38 -0400

    Friday updates
    Friday updates

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 // console.log('Hi there!');
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '8187c43',
-            s: 'avengers'
+            s: searchTerm
+ //           i: 'tt0848228'
         }
     }); 
 
     console.log(response.data);
 }; 
 
-fetchData();
+const input = document.querySelector('input'); 
+
+const debounce = (func, delay = 1000) => {
+    let timeoutId; 
+    return (...args) => {
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+            }, delay);
+    };
+};
+
+const onInput = event => { 
+    fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500)); 
+
