fc7fee3922989d223350dbf970e41b9c51522dc1
Gene Belford <35238105+EJBelford@users.noreply.github.com>
2022-09-03 13:15:21 -0400

    Changes from 9/3
    Changes from 9/3

--- utils.js
+++ utils.js
@@ -4,34 +4,21 @@
 //                Copyright, Belford DBA Consulting, LLC, 2022
 //                      Unpublished, All Rights Reserved
 //==============================================================================
+//
+//
 //--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
+//
 //
 //--*----1----*----2----*----3----*----4----*----5----*----6----*----7----*----8
 //
 //
 //--*----|----*----|----*----|----*----|----*----|----*----|----*----|----*----/
 
-// console.log('Hi there!');
-
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '8187c43',
-            s: searchTerm
- //           i: 'tt0848228'
-        }
-    }); 
-
-    console.log(response.data);
-}; 
-
-/*----------------*/
-
-const input = document.querySelector('input'); 
-
-const onInput = event => { 
-    fetchData(event.target.value);
+const debounce = (func, delay = 1000) => {
+    let timeoutId; 
+    return (...args) => {
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+            }, delay);
+    };
 };
-
-input.addEventListener('input', debounce(onInput, 500)); 
-
