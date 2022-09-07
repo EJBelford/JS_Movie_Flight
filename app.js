2a6207b862afd235d9001b88b92317068149b3c4
Gene Belford <35238105+EJBelford@users.noreply.github.com>
2022-09-07 17:28:17 -0400

    Update app.js
    updates for 9/7

--- app.js
+++ app.js
@@ -17,7 +17,73 @@ function throwDice(numRolls) {
         let roll = rollDie(6);
         console.log(`Rolled: ${roll}`);
     }
+} 
+
+function isValidPassword(pwd, userId) { 
+    if (pwd.length < 8) {
+        return false;
+    }
+    else if (pwd.includes(" ")) {
+        return false;
+    } 
+    else if (pwd.includes(userId)) {
+        return false; 
+    }
+    return true;
+} 
+
+function sleep(miliseconds) {
+    var currentTime = new Date().getTime();
+ 
+    while (currentTime + miliseconds >= new Date().getTime()) {
+    }
 }
 
+function pick(arr) {
+    return arr[random(arr.length)];
+}
+
+function avg(nums) {
+    let avg = 0;
+    for (let num of nums) {
+        avg += num; 
+    }
+    return avg/nums.length;
+}
+
+function getCard() { 
+    const cards = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+    const suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds'];
+
+    return { card: pick(cards), suit: pick(suits) };
+}
+
+console.log(getCard());
+sleep(50);
+console.log(getCard());
+sleep(50);
+console.log(getCard());
+sleep(50);
+console.log(getCard());
+sleep(50);
+console.log(getCard());
+
+/* let avgNum = avg([1, 2]);
+console.log(avgNum);
+avgNum = avg([1, 2, 3]);
+console.log(avgNum);
+avgNum = avg([1, 2, -6]);
+console.log(avgNum);
+avgNum = avg([]); 
+console.log(avgNum); */
+
+/* console.log(isValidPassword('pass', 'EJBelford')); 
+console.log(isValidPassword('password', 'EJBelford')); 
+console.log(isValidPassword('pass word', 'EJBelford')); 
+console.log(isValidPassword('password', 'EJBelford')); 
+console.log(isValidPassword('password', 'Password')); 
+console.log(isValidPassword('password', 'EJBelford'));
+console.log(isValidPassword('password', 'password'));   */
+
 // random(6);
-throwDice(2);
+// throwDice(2);
